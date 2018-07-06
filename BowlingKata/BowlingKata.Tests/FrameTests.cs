4d1c6a6de3258cbf14f9c84634bb8f3ccd5b0ba3
Alin Dicu <alinddicu@yahoo.com>
2018-07-06 21:27:28 +0200

    Frame is tested

--- BowlingKata/BowlingKata.Tests/FrameTests.cs
+++ BowlingKata/BowlingKata.Tests/FrameTests.cs
@@ -9,12 +9,37 @@
 		[TestMethod]
 		public void TestScore1AndScore2()
 		{
-			var frame = new Frame("5/4");
+			TestFrame(new Frame("5/4"), 5,4, false, false, 9);
+		}
 
-			Check.That(frame.Score1).IsEqualTo(5);
-			Check.That(frame.Score2).IsEqualTo(4);
-			Check.That(frame.IsSpare).IsFalse();
-			Check.That(frame.IsStrike).IsFalse();
+		[TestMethod]
+		public void TestMiss()
+		{
+			TestFrame(new Frame("-/-"), 0, 0, false, false, 0);
+			TestFrame(new Frame("1/-"), 1, 0, false, false, 1);
+			TestFrame(new Frame("-/2"), 0, 2, false, false, 2);
+		}
+
+		[TestMethod]
+		public void TestSpare()
+		{
+			TestFrame(new Frame("6/4"), 6, 4, true, false, 10);
+			TestFrame(new Frame("-/10"), 0, 10, true, false, 10);
+		}
+
+		[TestMethod]
+		public void TestStrike()
+		{
+			TestFrame(new Frame("10/-"), 10, 0, false, true, 10);
+		}
+
+		private static void TestFrame(Frame frame, int score1, int score2, bool isSpare, bool isStrike, int total)
+		{
+			Check.That(frame.Score1).IsEqualTo(score1);
+			Check.That(frame.Score2).IsEqualTo(score2);
+			Check.That(frame.IsSpare).IsEqualTo(isSpare);
+			Check.That(frame.IsStrike).IsEqualTo(isStrike);
+			Check.That(frame.Total).IsEqualTo(total);
 		}
 	}
 }
--- BowlingKata/BowlingKata/Frame.cs
+++ BowlingKata/BowlingKata/Frame.cs
@@ -2,17 +2,26 @@
 {
 	public class Frame
 	{
+		private const string Miss = "-";
+
 		public Frame(string frameResult)
 		{
-			var scoresStr = frameResult.Split('/');
+			var scores = frameResult.Split('/');
+			var score1 = scores[0];
+			var score2 = scores[1];
 
-			Score1 = int.Parse(scoresStr[0]);
-			Score2 = int.Parse(scoresStr[1]);
+			Score1 = score1 == Miss ? 0 : int.Parse(score1);
+			Score2 = score2 == Miss ? 0 : int.Parse(score2);
+
+			IsSpare = Score1 + Score2 == 10 && Score2 > 0;
+			IsStrike = Score1 == 10;
 		}
 
 		public int Score1 { get; }
 
 		public int Score2 { get; }
+
+		public int Total => Score1 + Score2;
 
 		public bool IsSpare { get; }
 
